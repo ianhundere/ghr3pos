920fe344f82d11734416ed143d62ff31394b6a6a
Ian Hundere <ianfundere@gmail.com>
2020-11-01 15:09:40 -0500

    Adds test_main_notfound test.

--- test_git.py
+++ test_git.py
@@ -18,20 +18,30 @@ def test_main(capsys):
     assert ghr3pos.main(types.SimpleNamespace(user="britannic", repo="blacklist", pullrequest=True, release=True)) is None
     assert capsys.readouterr() == ('\nThe last 3 pull requests for britannic in the blacklist repo are:\n\n\tTitle: Update README.md\n\tNumber: 18\n\n\tTitle: Updated script referenced in build\n\tNumber: 17\n\n\tTitle: Corrected script name referenced\n\tNumber: 16\n\nThe last 3 releases/versions for britannic in the blacklist repo are:\n\n\tRelease: Release 1.2.4.2 (August 14, 2020)\n\tVersion: v1.2.4.2\n\n\tRelease: Release 1.2.4.1 (August 10, 2020)\n\tVersion: v1.2.4.1\n\n\tRelease: Release 1.2.4.0 (August 9, 2020)\n\tVersion: v1.2.4.0\n','')
 
-def test_process_pr(capsys):
-    user = 'britannic'
-    repo = 'blacklist'
-    git = Git(user, repo)
-    data = pr_data
-    git.process_pr(data)
-    captured = capsys.readouterr()
-    assert captured.out.startswith(f'\nThe last 3 pull requests for {user} in the {repo} repo are:\n')
+def test_main_notfound(capsys):
+    assert ghr3pos.main(types.SimpleNamespace(user="cindy", repo="lauper", pullrequest=True, release=True)) is None
+    assert capsys.readouterr() == ('','\nerror: unable to retrieve pull requests because either the username, cindy, and/or repo, lauper, do not exist.\n\nerror: unable to retrieve releases because either the username, cindy, and/or repo, lauper, do not exist.\n')
 
-def test_process_rel(capsys):
-    user = 'britannic'
-    repo = 'blacklist'
-    git = Git(user, repo)
-    data = rel_data
-    git.process_rel(data)
-    captured = capsys.readouterr()
-    assert captured.out.startswith(f'\nThe last 3 releases/versions for {user} in the {repo} repo are:\n')
+
+# def test_get_pr(capsys):
+
+
+# def test_get_rel(capsys):
+
+# def test_process_pr(capsys):
+#     user = 'britannic'
+#     repo = 'blacklist'
+#     git = Git(user, repo)
+#     data = pr_data
+#     git.process_pr(data)
+#     captured = capsys.readouterr()
+#     assert captured.out.startswith(f'\nThe last 3 pull requests for {user} in the {repo} repo are:\n')
+
+# def test_process_rel(capsys):
+#     user = 'britannic'
+#     repo = 'blacklist'
+#     git = Git(user, repo)
+#     data = rel_data
+#     git.process_rel(data)
+#     captured = capsys.readouterr()
+#     assert captured.out.startswith(f'\nThe last 3 releases/versions for {user} in the {repo} repo are:\n')
