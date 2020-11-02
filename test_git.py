53fb6d6a439891a2ce343628ef491bf73b04edde
Ian Hundere <ianfundere@gmail.com>
2020-11-01 19:05:22 -0500

    Adds final test for 99% coverage..ouf, testing is hard.

--- test_git.py
+++ test_git.py
@@ -4,37 +4,63 @@ import pytest
 import sys
 import types
 
-def test_sanity():
-  assert True is True
 
 @pytest.mark.vcr()
 def test_main(capsys):
-    assert ghr3pos.main(['--user=britannic', '--repo=blacklist', '--release', '--pullrequest']) == 0
-    assert capsys.readouterr() == ('\nThe last 3 pull requests for britannic in the blacklist repo are:\n\n\tTitle: Update README.md\n\tNumber: 18\n\n\tTitle: Updated script referenced in build\n\tNumber: 17\n\n\tTitle: Corrected script name referenced\n\tNumber: 16\n\nThe last 3 releases/versions for britannic in the blacklist repo are:\n\n\tRelease: Release 1.2.4.2 (August 14, 2020)\n\tVersion: v1.2.4.2\n\n\tRelease: Release 1.2.4.1 (August 10, 2020)\n\tVersion: v1.2.4.1\n\n\tRelease: Release 1.2.4.0 (August 9, 2020)\n\tVersion: v1.2.4.0\n','')
+    assert ghr3pos.main(
+        ['--user=britannic', '--repo=blacklist', '--release', '--pullrequest']) == 0
+    assert capsys.readouterr() == ('\nThe last 3 pull requests for britannic in the blacklist repo are:\n\n\tTitle: Update README.md\n\tNumber: 18\n\n\tTitle: Updated script referenced in build\n\tNumber: 17\n\n\tTitle: Corrected script name referenced\n\tNumber: 16\n\nThe last 3 releases/versions for britannic in the blacklist repo are:\n\n\tRelease: Release 1.2.4.2 (August 14, 2020)\n\tVersion: v1.2.4.2\n\n\tRelease: Release 1.2.4.1 (August 10, 2020)\n\tVersion: v1.2.4.1\n\n\tRelease: Release 1.2.4.0 (August 9, 2020)\n\tVersion: v1.2.4.0\n', '')
+
+
 @pytest.mark.vcr()
 def test_main_user_not_found(capsys):
-    assert ghr3pos.main(['--user=ianfundere', '--repo=rpi-k3s', '--pullrequest']) == 0
-    assert capsys.readouterr() == ('','\nerror: unable to retrieve pull requests because either the username, ianfundere, and/or repo, rpi-k3s, do not exist.\n')
+    assert ghr3pos.main(
+        ['--user=ianfundere', '--repo=rpi-k3s', '--pullrequest']) == 0
+    assert capsys.readouterr() == (
+        '', '\nerror: unable to retrieve pull requests because either the username, ianfundere, and/or repo, rpi-k3s, do not exist.\n')
+
+
 @pytest.mark.vcr()
 def test_main_repo_not_found(capsys):
-    assert ghr3pos.main(['--user=ianhundere', '--repo=lauper', '--release']) == 0
-    assert capsys.readouterr() == ('','\nerror: unable to retrieve releases because either the username, ianhundere, and/or repo, lauper, do not exist.\n')
+    assert ghr3pos.main(
+        ['--user=ianhundere', '--repo=lauper', '--release']) == 0
+    assert capsys.readouterr() == (
+        '', '\nerror: unable to retrieve releases because either the username, ianhundere, and/or repo, lauper, do not exist.\n')
+
+
 @pytest.mark.vcr()
 def test_main_no_rel(capsys):
-    assert ghr3pos.main(['--user=ianhundere', '--repo=ran-jam', '--release']) == 0
-    assert capsys.readouterr() == ('\nNo releases available\n','')
+    assert ghr3pos.main(
+        ['--user=ianhundere', '--repo=ran-jam', '--release']) == 0
+    assert capsys.readouterr() == ('\nNo releases available\n', '')
+
+
 @pytest.mark.vcr()
 def test_main_no_pr(capsys):
-    assert ghr3pos.main(['--user=ianhundere', '--repo=scales-o-rama', '--pullrequest']) == 0
-    assert capsys.readouterr() == ('\nNo pull requests available\n','')
+    assert ghr3pos.main(
+        ['--user=ianhundere', '--repo=scales-o-rama', '--pullrequest']) == 0
+    assert capsys.readouterr() == ('\nNo pull requests available\n', '')
+
+
 @pytest.mark.vcr()
 def test_main_pr_ratelimited(capsys):
-    assert ghr3pos.main(['--user=ianhundere', '--repo=scales-o-rama', '--pullrequest']) == 0
-    assert capsys.readouterr() == ('\n[PULLREQUEST] You\'ve hit your API rate limit, refer to https://developer.github.com/v3/#rate-limiting for more information.\n','')
+    assert ghr3pos.main(
+        ['--user=ianhundere', '--repo=scales-o-rama', '--pullrequest']) == 0
+    assert capsys.readouterr() == (
+        '\n[PULLREQUEST] You\'ve hit your API rate limit, refer to https://developer.github.com/v3/#rate-limiting for more information.\n', '')
+
+
 @pytest.mark.vcr()
 def test_main_rel_ratelimited(capsys):
-    assert ghr3pos.main(['--user=ianhundere', '--repo=scales-o-rama', '--release']) == 0
-    assert capsys.readouterr() == ('\n[RELEASE] You\'ve hit your API rate limit, refer to https://developer.github.com/v3/#rate-limiting for more information.\n','')
+    assert ghr3pos.main(
+        ['--user=ianhundere', '--repo=scales-o-rama', '--release']) == 0
+    assert capsys.readouterr() == (
+        '\n[RELEASE] You\'ve hit your API rate limit, refer to https://developer.github.com/v3/#rate-limiting for more information.\n', '')
+
+
 def test_main_no_flags(capsys):
-    assert ghr3pos.main(['--user=ianhundere', '--repo=scales-o-rama']) == 0
-    assert capsys.readouterr() == ('','')
+    with pytest.raises(SystemExit) as exc_info:
+        ghr3pos.main(['--user=ianhundere', '--repo=scales-o-rama'])
+    assert exc_info.value.args == (2,)
+    assert capsys.readouterr() == (
+        '', 'usage: pytest [-h] [-u USER] [-r REPO] [-R RELEASE] [-P PR]\npytest: error: no --release (-R) or --pullrequest (-P) flag given.\n')
